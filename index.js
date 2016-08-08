986c6c488baab96a4eb74fc751628fd498420eb7
loveKino <842914439@qq.com>
2016-08-08 17:40:44 +0800

    init

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let shadowFrame = require('./lib/shadowFrame');
+
+let getX = (elem) => {
+    var x = 0;
+    while (elem) {
+        x = x + elem.offsetLeft;
+        elem = elem.offsetParent;
+    }
+    return x;
+};
+
+let getY = (elem) => {
+    var y = 0;
+    while (elem) {
+        y = y + elem.offsetTop;
+        elem = elem.offsetParent;
+    }
+    return y;
+};
+
+let getClientX = (elem) => {
+    return getX(elem) - window.scrollX;
+};
+
+let getClientY = (elem) => {
+    return getY(elem) - window.scrollY;
+};
+
+let removeChilds = (node) => {
+    while (node && node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+};
+
+let once = (node, type, handler, useCapture) => {
+    let fun = function(e) {
+        let ret = handler.apply(this, [e]);
+        node.removeEventListener(type, fun, useCapture);
+        return ret;
+    };
+
+    node.addEventListener(type, fun, useCapture);
+};
+
+module.exports = {
+    getX,
+    getY,
+    getClientX,
+    getClientY,
+    removeChilds,
+    once,
+    shadowFrame
+};
